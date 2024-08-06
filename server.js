1f0d8f8fe38bc284565c780258a7901f06ed8de2
LenaDurant <durant.arlena@gmail.com>
2024-08-05 22:46:48 -0400

    updated server.js and connected to browser

--- server.js
+++ server.js
@@ -1,14 +1,45 @@
-const exporess = require ('express');
+const express = require ('express');
 const app = express();
+
+app.use(express.json());
 
 app.get('/', (req, res, next) => {
     res.send(`<h1>Bucket List!</h1>`);
 });
 
-app.get('/bunnies', (req, res, next) => {
-    res.send(`Bucket Kicked...</h1>`);
+app.get('/employees', (req, res, next) => {
+    res.send([
+        {
+          id: 1,
+					name: "Bob Evans",
+					admin: true,
+        },
+
+				{
+          id: 2,
+					name: "Taco Bowdet",
+					admin: false,
+        },
+
+				{
+          id: 3,
+					name: "Burger Queen",
+					admin: true,
+        },
+				
+				{
+          id: 4,
+					name: "Pandemic Bread",
+					admin: false,
+        },
+
+				{
+          id: 5,
+					name: "Chick Felicia",
+					admin: true,
+        },
+    ])
 });
 
-app.listen(3000, () => { console.log(`listening on port`)
-
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => { console.log(`listening on port ${PORT}`)});
